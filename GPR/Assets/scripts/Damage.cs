04acb7ad044159e8379b1b530068b5029f08930d
29244 <29244@ma-web.nl>
2021-02-12 11:23:08 +0100

    1

---
+++ GPR/Assets/scripts/Damage.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Damage : MonoBehaviour
+{
+    protected int Health = 100;
+    [SerializeField] Text hitText;
+
+    void Update()
+    {
+        if (Health == 0)
+        {
+            Destroy(gameObject);
+            Debug.Log("Dead");
+            Time.timeScale = 0;
+        }
+
+        if (Input.GetKeyDown("Space"))
+        {
+            Hit();
+        }
+
+    }
+    void Hit()
+    {
+        Health -= 10;
+        Debug.Log(Health);
+        hitText.text = "HP:" + " " + Health.ToString();
+        transform.position = new Vector3(-5, 0, 0);
+    }
+}
